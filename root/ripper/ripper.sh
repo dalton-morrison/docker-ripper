26c6e85a63daa09f1a73e7cad29ff620c0651b3d
Dalton Morrison <abemorrison48@gmail.com>
2022-02-09 01:15:31 -0500

    Moved permissions changes to occur before the files are moved to the finished directory.

--- root/ripper/ripper.sh
+++ root/ripper/ripper.sh
@@ -82,14 +82,15 @@ if [ "$BD1" = 'DRV:0,2,999,12,"' ] || [ "$BD2" = 'DRV:0,2,999,28,"' ]; then
     echo "$(date "+%d.%m.%Y %T") : BluRay detected: Saving MKV"
     makemkvcon --profile=/config/default.mmcp.xml -r --decrypt --minlength=600 mkv disc:"$BLURAYNUM" all "$BDPATH" >> $LOGFILE 2>&1
  fi
+ # permissions
+ chown -R nobody:users "$STORAGE_BD" && chmod -R g+rw "$STORAGE_BD"
  if [ "$SEPARATERAWFINISH" = 'true' ]; then
     BDFINISH="$STORAGE_BD"/finished/
     mv -v "$BDPATH" "$BDFINISH"
  fi
  echo "$(date "+%d.%m.%Y %T") : Done! Ejecting Disk"
  eject $DRIVE >> $LOGFILE 2>&1
- # permissions
- chown -R nobody:users "$STORAGE_BD" && chmod -R g+rw "$STORAGE_BD"
+ 
 fi
 
 if [ "$DVD" = 'DRV:0,2,999,1,"' ]; then
@@ -106,14 +107,15 @@ if [ "$DVD" = 'DRV:0,2,999,1,"' ]; then
     echo "$(date "+%d.%m.%Y %T") : DVD detected: Saving MKV"
     makemkvcon --profile=/config/default.mmcp.xml -r --decrypt --minlength=600 mkv disc:"$DVDNUM" all "$DVDPATH" >> $LOGFILE 2>&1
  fi
+ # permissions
+ chown -R nobody:users "$STORAGE_DVD" && chmod -R g+rw "$STORAGE_DVD"
  if [ "$SEPARATERAWFINISH" = 'true' ]; then
     DVDFINISH="$STORAGE_DVD"/finished/
     mv -v "$DVDPATH" "$DVDFINISH" 
  fi
  echo "$(date "+%d.%m.%Y %T") : Done! Ejecting Disk"
  eject $DRIVE >> $LOGFILE 2>&1
- # permissions
- chown -R nobody:users "$STORAGE_DVD" && chmod -R g+rw "$STORAGE_DVD"
+ 
 fi
 
 if [ "$CD1" = 'DRV:0,2,999,0,"' ]; then
